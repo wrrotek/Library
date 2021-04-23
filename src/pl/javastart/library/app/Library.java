92b7352764491047ff80401c73f208db4edc7201
Kornelia Chilczuk Kudan <kornelia.chilczuk@gmail.com>
2021-04-23 19:35:18 +0200

    5th part of the Library project

--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -1,6 +1,9 @@
+package pl.javastart.library.app;
+import pl.javastart.library.model.Book;
+
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.4";
+        final String appName = "Biblioteka v0.5";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
                 296, "Greg", "9788373271890");
